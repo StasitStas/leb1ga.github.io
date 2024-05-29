a8d34f0224f6890df50b64b19b5fd3a89592cb0c
StasitStas <116554529+StasitStas@users.noreply.github.com>
2024-05-29 16:17:51 +0300

    Update about.js

--- about.js
+++ about.js
@@ -2,18 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const exchangeButton = document.getElementById('exchangeButton');
     const mineButton = document.getElementById('mineButton');
 
-    // Підключення до Firebase
-    const firebaseConfig = {
-        apiKey: "AIzaSyApLBsJ9AOXu7i38Rm_t4cGuEgxpZRDaeE",
-        authDomain: "leb1gabot.firebaseapp.com",
-        projectId: "leb1gabot",
-        storageBucket: "leb1gabot.appspot.com",
-        messagingSenderId: "1096597237881",
-        appId: "1:1096597237881:web:29b0383c26ed9acfcaa4a8",
-        measurementId: "G-Z1KFE8C0ML"
-    };
-    firebase.initializeApp(firebaseConfig);
-    const db = firebase.firestore();
 
     navButtons.forEach(button => {
         button.addEventListener('click', function() {
