69651b347be067825d652d36f28060d731c3eb39
StasitStas <116554529+StasitStas@users.noreply.github.com>
2024-06-10 18:44:20 +0300

    Update about.js

--- about.js
+++ about.js
@@ -1,8 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const settingsIcon = document.getElementById('settingsIcon');
-    const settingsWindow = document.getElementById('settingsWindow');
-    const animationToggle = document.getElementById('animationToggle');
-    const vibrationToggle = document.getElementById('vibrationToggle');
     const exchangeButton = document.getElementById('exchangeButton');
     const mineButton = document.getElementById('mineButton');
     const friendsButton = document.getElementById('friendsButton');
@@ -13,42 +9,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let username = '';
     let firstName = '';
-    let enableAnimation = true;
-    let enableVibration = true;
     let linkMain = '';
     let linkAbout = '';
     let linkFriends = '';
     let linkEarn = '';
     let linkDrop = '';
-
-    let settingsWindowOpen = false;
-
-    settingsIcon.addEventListener('click', function(event) {
-        event.stopPropagation();
-        settingsWindow.style.display = settingsWindowOpen ? 'none' : 'block';
-        settingsWindowOpen = !settingsWindowOpen;
-    });
-
-    document.addEventListener('click', function() {
-        if (settingsWindowOpen) {
-            settingsWindow.style.display = 'none';
-            settingsWindowOpen = false;
-        }
-    });
-
-    settingsWindow.addEventListener('click', function(event) {
-        event.stopPropagation();
-    });
-
-    animationToggle.addEventListener('change', function() {
-        enableAnimation = animationToggle.checked;
-        saveSettings();
-    });
-
-    vibrationToggle.addEventListener('change', function() {
-        enableVibration = vibrationToggle.checked;
-        saveSettings();
-    });
 
     function getUsernameFromUrl() {
         const urlParams = new URLSearchParams(window.location.search);
@@ -76,19 +41,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 linkEarn = userData.link_earn;
                 linkDrop = userData.link_drop;
                 usernameDisplay.textContent = firstName;
-                db.collection("clicks").doc(username).get().then(doc => {
-                    if (doc.exists) {
-                        const data = doc.data();
-                        enableAnimation = data.enableAnimation !== undefined ? data.enableAnimation : true;
-                        enableVibration = data.enableVibration !== undefined ? data.enableVibration : true;
-                        animationToggle.checked = enableAnimation;
-                        vibrationToggle.checked = enableVibration;
-                    } else {
-                        db.collection("clicks").doc(username).set({ enableAnimation: true, enableVibration: true });
-                    }
-                }).catch(error => {
-                    console.error("Error getting document:", error);
-                });
             }).catch(error => {
                 console.error("Error getting user data:", error);
                 alert('Помилка: Не вдалося отримати дані користувача.');
@@ -96,15 +48,6 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             alert('Помилка: Ім\'я користувача не вказане.');
         }
-    }
-
-    function saveSettings() {
-        db.collection("clicks").doc(username).set({
-            enableAnimation,
-            enableVibration
-        }).catch(error => {
-            console.error("Помилка оновлення документа:", error);
-        });
     }
 
     navButtons.forEach(button => {
