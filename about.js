b1c1c7a87fddf7d79c332c981574c116bafa49c5
StasitStas <116554529+StasitStas@users.noreply.github.com>
2024-06-16 14:42:05 +0300

    Update about.js

--- about.js
+++ about.js
@@ -11,6 +11,11 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeModalButton = document.querySelector('.close-modal');
     const cofferImage = document.getElementById('coffer-image');
     const openCofferButton = document.getElementById('open-coffer');
+    const prizeModal = document.getElementById('prize-modal');
+    const closePrizeModalButton = document.querySelector('.close-prize-modal');
+    const prizeDescription = document.getElementById('prize-description');
+    const prizeImage = document.getElementById('prize-image');
+    const claimPrizeButton = document.getElementById('claim-prize');
 
     let username = '';
     let firstName = '';
@@ -120,7 +125,53 @@ document.addEventListener('DOMContentLoaded', function() {
     openCofferButton.addEventListener('click', function() {
         cofferModal.style.display = 'none';
         document.body.classList.remove('modal-open');
+        generatePrize();
     });
+
+    closePrizeModalButton.addEventListener('click', function() {
+        prizeModal.style.display = 'none';
+        document.body.classList.remove('modal-open');
+    });
+
+    claimPrizeButton.addEventListener('click', function() {
+        prizeModal.style.display = 'none';
+        document.body.classList.remove('modal-open');
+        // Implement prize claiming logic here if needed
+    });
+
+    function generatePrize() {
+        const clickPrizeProbability = 0.5;
+        const skinPrizeProbability = 0.5;
+        const skins = [
+            { src: 'leb1ga-ment.png', probability: 0.7 },
+            { src: 'skin_1.png', probability: 0.5 },
+            { src: 'skin_2.png', probability: 0.25 }
+        ];
+
+        let prizeDescriptionText = '';
+        let prizeImageSrc = '';
+        
+        if (Math.random() < clickPrizeProbability) {
+            const clicks = Math.floor(Math.random() * (150 - 30 + 1)) + 30;
+            prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
+            prizeImageSrc = 'coin.png';
+        } else {
+            const skin = skins.find(skin => Math.random() < skin.probability);
+            if (skin) {
+                prizeDescriptionText = 'Ваш приз: Скін';
+                prizeImageSrc = skin.src;
+            } else {
+                prizeDescriptionText = 'Ваш приз: Скін';
+                prizeImageSrc = skins[2].src; // Default skin if none matched
+            }
+        }
+
+        // Update the prize modal with the generated prize
+        prizeDescription.textContent = prizeDescriptionText;
+        prizeImage.src = prizeImageSrc;
+        prizeModal.style.display = 'flex';
+        document.body.classList.add('modal-open');
+    }
 
     initialize();
 });
