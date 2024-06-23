59350bf1f05edb7b481c4c4711ff1208d13f6455
StasitStas <116554529+StasitStas@users.noreply.github.com>
2024-06-23 16:44:56 +0300

    Update drop.js

--- drop.js
+++ drop.js
@@ -1,20 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const exchangeButton = document.getElementById('exchangeButton');
-    const mineButton = document.getElementById('mineButton');
-    const friendsButton = document.getElementById('friendsButton');
-    const earnButton = document.getElementById('earnButton');
-    const airdropButton = document.getElementById('airdropButton');
-    const usernameDisplay = document.getElementById('usernameDisplay');
     const leaderboardList = document.getElementById('leaderboardList');
     const navButtons = document.querySelectorAll('.nav-button');
 
     let username = '';
     let firstName = '';
-    let linkMain = '';
-    let linkAbout = '';
-    let linkFriends = '';
-    let linkEarn = '';
-    let linkDrop = '';
 
     function getUsernameFromUrl() {
         const urlParams = new URLSearchParams(window.location.search);
@@ -36,12 +25,6 @@ document.addEventListener('DOMContentLoaded', function() {
         if (username) {
             getUserData(username).then(userData => {
                 firstName = userData.first_name;
-                linkMain = userData.link_main;
-                linkAbout = userData.link_about;
-                linkFriends = userData.link_friends;
-                linkEarn = userData.link_earn;
-                linkDrop = userData.link_drop;
-                usernameDisplay.textContent = firstName;
                 updateLeaderboard(); // Оновити рейтинг при завантаженні
             }).catch(error => {
                 console.error("Error getting user data:", error);
@@ -85,46 +68,5 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    exchangeButton.addEventListener('click', function() {
-        if (linkMain) {
-            window.location.href = linkMain;
-        } else {
-            alert('Помилка: Посилання не знайдено.');
-        }
-    });
-
-    mineButton.addEventListener('click', function() {
-        if (linkAbout) {
-            window.location.href = linkAbout;
-        } else {
-            window.location.href = 'about.html';
-        }
-    });
-
-    friendsButton.addEventListener('click', function() {
-        if (linkFriends) {
-            window.location.href = linkFriends;
-        } else {
-            alert('Помилка: Посилання не знайдено.');
-        }
-    });
-
-    earnButton.addEventListener('click', function() {
-        if (linkEarn) {
-            window.location.href = linkEarn;
-        } else {
-            alert('Помилка: Посилання не знайдено.');
-        }
-    });
-
-    airdropButton.addEventListener('click', function() {
-        if (linkDrop) {
-            window.location.href = linkDrop;
-        } else {
-            alert('Помилка: Посилання не знайдено.');
-        }
-    });
-
     initialize();
 });
-
