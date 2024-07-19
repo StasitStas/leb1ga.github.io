2afc3edafa3e991c7f4393ed4c7a2299043779cc
StasitStas <116554529+StasitStas@users.noreply.github.com>
2024-07-19 18:16:48 +0300

    Update about.js

--- about.js
+++ about.js
@@ -52,8 +52,8 @@ document.addEventListener('DOMContentLoaded', function() {
         skinsContainer.innerHTML = '';
     
         const sortedSkins = Object.keys(skins).sort((a, b) => {
-            const numA = parseInt(a.split('_')[1], 10);
-            const numB = parseInt(b.split('_')[1], 10);
+            const numA = parseInt(a.split('_')[1], 20);
+            const numB = parseInt(b.split('_')[1], 20);
             return numA - numB;
         });
     
@@ -149,13 +149,26 @@ document.addEventListener('DOMContentLoaded', function() {
             cofferModal.style.display = 'flex';
             document.body.classList.add('modal-open');
             currentCoffer = item.id;
-
-            if (currentCoffer === 'coffer1') {
-                cofferPrice.textContent = '100';
-            } else if (currentCoffer === 'coffer2') {
-                cofferPrice.textContent = '800';
-            } else if (currentCoffer === 'coffer3') {
-                cofferPrice.textContent = '5000';
+    
+            switch (currentCoffer) {
+                case 'coffer1':
+                    cofferPrice.textContent = '50';
+                    break;
+                case 'coffer2':
+                    cofferPrice.textContent = '500';
+                    break;
+                case 'coffer3':
+                    cofferPrice.textContent = '5000';
+                    break;
+                case 'coffer4':
+                    cofferPrice.textContent = '10000';
+                    break;
+                case 'coffer5':
+                    cofferPrice.textContent = '50000';
+                    break;
+                case 'coffer6':
+                    cofferPrice.textContent = '300000';
+                    break;
             }
         });
     });
@@ -167,14 +180,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
     openCofferButton.addEventListener('click', async function() {
         let cofferCost = 0;
-        if (currentCoffer === 'coffer1') {
-            cofferCost = 100;
-        } else if (currentCoffer === 'coffer2') {
-            cofferCost = 800;
-        } else if (currentCoffer === 'coffer3') {
-            cofferCost = 5000;
+        switch (currentCoffer) {
+            case 'coffer1':
+                cofferCost = 50;
+                break;
+            case 'coffer2':
+                cofferCost = 500;
+                break;
+            case 'coffer3':
+                cofferCost = 5000;
+                break;
+            case 'coffer4':
+                cofferCost = 10000;
+                break;
+            case 'coffer5':
+                cofferCost = 50000;
+                break;
+            case 'coffer6':
+                cofferCost = 300000;
+                break;
         }
-
+    
         if (clickCount >= cofferCost) {
             clickCount -= cofferCost;
             await db.collection("clicks").doc(username).update({ clickCount: clickCount });
@@ -202,93 +228,130 @@ document.addEventListener('DOMContentLoaded', function() {
     async function generatePrize(cofferType) {
         let prizeDescriptionText = '';
         let prizeImageSrc = '';
-
-        if (cofferType === 'coffer1') {
-            const clickPrizeProbability = 0.5;
-            const skins = [
-                { src: 'skin/skin_1.png', id: 'skin_1', probability: 0.7 },
-                { src: 'skin/skin_2.png', id: 'skin_2', probability: 0.5 },
-                { src: 'skin/skin_3.png', id: 'skin_3', probability: 0.25 }
-            ];
-
-            if (Math.random() < clickPrizeProbability) {
-                const clicks = Math.floor(Math.random() * (150 - 30 + 1)) + 30;
-                clickCount += clicks;
-                await db.collection("clicks").doc(username).update({ clickCount: clickCount });
-                prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
-                prizeImageSrc = 'coin.png';
-            } else {
-                const skin = skins.find(skin => Math.random() < skin.probability);
-                if (skin) {
+    
+        switch (cofferType) {
+            case 'coffer1':
+                if (Math.random() < 0.5) {
+                    const clicks = Math.floor(Math.random() * (100 - 10 + 1)) + 10;
+                    clickCount += clicks;
+                    await db.collection("clicks").doc(username).update({ clickCount: clickCount });
+                    prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
+                    prizeImageSrc = 'coin.png';
+                } else {
+                    const skins = [
+                        { src: 'skin/skin_1.png', id: 'skin_1', probability: 0.5 },
+                        { src: 'skin/skin_2.png', id: 'skin_2', probability: 0.4 },
+                        { src: 'skin/skin_3.png', id: 'skin_3', probability: 0.3 },
+                        { src: 'skin/skin_4.png', id: 'skin_4', probability: 0.25 }
+                    ];
+                    const skin = skins.find(s => Math.random() < s.probability);
                     await updateSkin(skin.id);
                     prizeDescriptionText = 'Ваш приз: Скін';
                     prizeImageSrc = skin.src;
-                } else {
-                    const defaultSkin = skins[2];
-                    await updateSkin(defaultSkin.id);
-                    prizeDescriptionText = 'Ваш приз: Скін';
-                    prizeImageSrc = defaultSkin.src;
                 }
-            }
-        } else if (cofferType === 'coffer2') {
-            const clickPrizeProbability = 0.6;
-            const skins = [
-                { src: 'skin/skin_4.png', id: 'skin_4', probability: 0.5 },
-                { src: 'skin/skin_5.png', id: 'skin_5', probability: 0.3 },
-                { src: 'skin/skin_6.png', id: 'skin_6', probability: 0.25 },
-                { src: 'skin/skin_7.png', id: 'skin_7', probability: 0.1 }
-            ];
-
-            if (Math.random() < clickPrizeProbability) {
-                const clicks = Math.floor(Math.random() * (1400 - 200 + 1)) + 200;
-                clickCount += clicks;
-                await db.collection("clicks").doc(username).update({ clickCount: clickCount });
-                prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
-                prizeImageSrc = 'coin.png';
-            } else {
-                const skin = skins.find(skin => Math.random() < skin.probability);
-                if (skin) {
+                break;
+            case 'coffer2':
+                if (Math.random() < 0.5) {
+                    const clicks = Math.floor(Math.random() * (800 - 100 + 1)) + 100;
+                    clickCount += clicks;
+                    await db.collection("clicks").doc(username).update({ clickCount: clickCount });
+                    prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
+                    prizeImageSrc = 'coin.png';
+                } else {
+                    const skins = [
+                        { src: 'skin/skin_5.png', id: 'skin_5', probability: 0.5 },
+                        { src: 'skin/skin_6.png', id: 'skin_6', probability: 0.4 },
+                        { src: 'skin/skin_7.png', id: 'skin_7', probability: 0.3 },
+                        { src: 'skin/skin_8.png', id: 'skin_8', probability: 0.25 }
+                    ];
+                    const skin = skins.find(s => Math.random() < s.probability);
                     await updateSkin(skin.id);
                     prizeDescriptionText = 'Ваш приз: Скін';
                     prizeImageSrc = skin.src;
-                } else {
-                    const defaultSkin = skins[3];
-                    await updateSkin(defaultSkin.id);
-                    prizeDescriptionText = 'Ваш приз: Скін';
-                    prizeImageSrc = defaultSkin.src;
                 }
-            }
-        } else if (cofferType === 'coffer3') {
-            const clickPrizeProbability = 0.5;
-            const skins = [
-                { src: 'skin/skin_8.png', id: 'skin_8', probability: 0.5 },
-                { src: 'skin/skin_9.png', id: 'skin_9', probability: 0.3 },
-                { src: 'skin/skin_10.png', id: 'skin_10', probability: 0.1 }
-            ];
-
-            if (Math.random() < clickPrizeProbability) {
-                const clicks = Math.floor(Math.random() * (10000 - 1000 + 1)) + 1000;
-                clickCount += clicks;
-                await db.collection("clicks").doc(username).update({ clickCount: clickCount });
-                prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
-                prizeImageSrc = 'coin.png';
-            } else {
-                const skin = skins.find(skin => Math.random() < skin.probability);
-                if (skin) {
+                break;
+            case 'coffer3':
+                if (Math.random() < 0.4) {
+                    const clicks = Math.floor(Math.random() * (8000 - 2000 + 1)) + 2000;
+                    clickCount += clicks;
+                    await db.collection("clicks").doc(username).update({ clickCount: clickCount });
+                    prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
+                    prizeImageSrc = 'coin.png';
+                } else {
+                    const skins = [
+                        { src: 'skin/skin_9.png', id: 'skin_9', probability: 0.5 },
+                        { src: 'skin/skin_10.png', id: 'skin_10', probability: 0.3 },
+                        { src: 'skin/skin_11.png', id: 'skin_11', probability: 0.2 }
+                    ];
+                    const skin = skins.find(s => Math.random() < s.probability);
                     await updateSkin(skin.id);
                     prizeDescriptionText = 'Ваш приз: Скін';
                     prizeImageSrc = skin.src;
-                } else {
-                    const defaultSkin = skins[2];
-                    await updateSkin(defaultSkin.id);
-                    prizeDescriptionText = 'Ваш приз: Скін';
-                    prizeImageSrc = defaultSkin.src;
                 }
-            }
+                break;
+            case 'coffer4':
+                if (Math.random() < 0.6) {
+                    const clicks = Math.floor(Math.random() * (15000 - 2000 + 1)) + 2000;
+                    clickCount += clicks;
+                    await db.collection("clicks").doc(username).update({ clickCount: clickCount });
+                    prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
+                    prizeImageSrc = 'coin.png';
+                } else {
+                    const skins = [
+                        { src: 'skin/skin_12.png', id: 'skin_12', probability: 0.5 },
+                        { src: 'skin/skin_13.png', id: 'skin_13', probability: 0.3 },
+                        { src: 'skin/skin_14.png', id: 'skin_14', probability: 0.2 }
+                    ];
+                    const skin = skins.find(s => Math.random() < s.probability);
+                    await updateSkin(skin.id);
+                    prizeDescriptionText = 'Ваш приз: Скін';
+                    prizeImageSrc = skin.src;
+                }
+                break;
+            case 'coffer5':
+                if (Math.random() < 0.4) {
+                    const clicks = Math.floor(Math.random() * (70000 - 30000 + 1)) + 30000;
+                    clickCount += clicks;
+                    await db.collection("clicks").doc(username).update({ clickCount: clickCount });
+                    prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
+                    prizeImageSrc = 'coin.png';
+                } else {
+                    const skins = [
+                        { src: 'skin/skin_15.png', id: 'skin_15', probability: 0.5 },
+                        { src: 'skin/skin_16.png', id: 'skin_16', probability: 0.3 },
+                        { src: 'skin/skin_17.png', id: 'skin_17', probability: 0.2 }
+                    ];
+                    const skin = skins.find(s => Math.random() < s.probability);
+                    await updateSkin(skin.id);
+                    prizeDescriptionText = 'Ваш приз: Скін';
+                    prizeImageSrc = skin.src;
+                }
+                break;
+            case 'coffer6':
+                if (Math.random() < 0.4) {
+                    const clicks = Math.floor(Math.random() * (500000 - 100000 + 1)) + 100000;
+                    clickCount += clicks;
+                    await db.collection("clicks").doc(username).update({ clickCount: clickCount });
+                    prizeDescriptionText = `Ваш приз: ${clicks} кліків`;
+                    prizeImageSrc = 'coin.png';
+                } else {
+                    const skins = [
+                        { src: 'skin/skin_18.png', id: 'skin_18', probability: 0.5 },
+                        { src: 'skin/skin_19.png', id: 'skin_19', probability: 0.3 },
+                        { src: 'skin/skin_20.png', id: 'skin_20', probability: 0.2 }
+                    ];
+                    const skin = skins.find(s => Math.random() < s.probability);
+                    await updateSkin(skin.id);
+                    prizeDescriptionText = 'Ваш приз: Скін';
+                    prizeImageSrc = skin.src;
+                }
+                break;
         }
-
-        prizeDescription.textContent = prizeDescriptionText;
-        prizeImage.src = prizeImageSrc;
+    
+        document.getElementById('prize-image').src = prizeImageSrc;
+        document.getElementById('prize-description').textContent = prizeDescriptionText;
+    
+        const prizeModal = document.getElementById('prize-modal');
         prizeModal.style.display = 'flex';
         document.body.classList.add('modal-open');
     }
@@ -318,12 +381,22 @@ document.addEventListener('DOMContentLoaded', function() {
             case 'skin_2': return 2;
             case 'skin_3': return 3;
             case 'skin_4': return 5;
-            case 'skin_5': return 5;
-            case 'skin_6': return 6;
-            case 'skin_7': return 8;
-            case 'skin_8': return 10;
-            case 'skin_9': return 15;
-            case 'skin_10': return 20;
+            case 'skin_5': return 10;
+            case 'skin_6': return 15;
+            case 'skin_7': return 17;
+            case 'skin_8': return 20;
+            case 'skin_9': return 20;
+            case 'skin_10': return 25;
+            case 'skin_11': return 30;
+            case 'skin_12': return 35;
+            case 'skin_13': return 40;
+            case 'skin_14': return 55;
+            case 'skin_15': return 60;
+            case 'skin_16': return 75;
+            case 'skin_17': return 80;
+            case 'skin_18': return 95;
+            case 'skin_19': return 100;
+            case 'skin_20': return 200;
             default: return 0;
         }
     }
